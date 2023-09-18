e4f06514fb9b68af66cfb374126b7c2f6adf4f48
DidiGG <ddgrisalesg@uqvirtual.edu.co>
2023-09-18 11:04:48 -0500

    Se agrega el medtodo en el main

--- src/main/java/co/uniquindio/proyecto/Application.java
+++ src/main/java/co/uniquindio/proyecto/Application.java
@@ -1,11 +1,11 @@
 package co.uniquindio.proyecto;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
+    @SpringBootApplication
+    public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+        }
     }
-}
